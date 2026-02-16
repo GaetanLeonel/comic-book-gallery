59271526c0b61aa3aaa781d20a7f1c6fbddd1c27
GaetanLeonel <lionelguigouon@gmail.com>
2026-02-16 13:26:14 +0100

    added Comic Book Detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         { 
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)  //en fonction du jour de la semaine, on redirige vers la page d'accueil
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the Comic Books controller!");
+            return View();
 
         }
     }
